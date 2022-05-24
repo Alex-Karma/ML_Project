f3ef3a6e1bdb6af97eb8f377c93090ad6231c1b7
Alex-Karma <Karmaev.alex2003@yandex.ru>
2022-05-24 05:06:44 +0300

    Final version

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # импортируем файл с функциями для работы программы
-import package
+from package import reading_photo, vertical_lines_detection, horizontal_lines_detection, combining_horizontals_and_verticals, sort_contours, form_cells, rows, rows_sort, recognition, data_to_excel
 
 
 # получаем считанное фото и фото в бинарных данных (с реверсом цвета и без шумов)
-img, img_bin = package.reading_photo("Photos/try4.png")
+origin, img, img_bin = reading_photo("Photos/tests/try6.png")
 
 # определяем длинну ядра для распознания горизонталей и вертикалей, как сотую часть длинны картинки
-kernel_len = np.array(img).shape[1]//100
+kernel_len = 5 * np.array(origin).shape[1]//100
 
 # получаем вертикали и горизонтали
-vertical_lines = package.vertical_lines_detection(kernel_len, img_bin)
-horizontal_lines = package.horizontal_lines_detection(kernel_len, img_bin)
+vertical_lines = vertical_lines_detection(kernel_len, img_bin)
+horizontal_lines = horizontal_lines_detection(kernel_len, img_bin)
 
 # получаем таблицу - объединениие верикалей и горизонталей
-img_vh = package.combining_horizontals_and_verticals(horizontal_lines, vertical_lines)
+img_vh = combining_horizontals_and_verticals(horizontal_lines, vertical_lines)
 
 # накладываем нашу отдельную табицу на фото
 bitxor = cv2.bitwise_xor(img, img_vh)
 bitnot = cv2.bitwise_not(bitxor)  # меняем цвета наоборот
 # выводим это наложение
-plotting = plt.imshow(bitnot, cmap='gray')
+plt.imshow(bitnot, cmap='gray')
 plt.show()
 
 # находим контуры в таблице, чтобы дальше получить точные координтаты каждой ячейки таблицы
@@ -33,13 +33,13 @@ plt.show()
 contours, hierarchy = cv2.findContours(img_vh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # получаем отсортированные контуры сверху вниз и их рамки
-contours, boundingBoxes = package.sort_contours(contours, method="top-to-bottom")
+contours, boundingBoxes = sort_contours(contours, method="top-to-bottom")
 
 # формируем ячейки в формате: вершина (x,y), длинна и ширина
-box = package.form_cells(contours, img)
+box = form_cells(contours, origin)
 
 # получем список всех ячеек по строкам (элементы в одинаковых идут подряд)
-row = package.rows(box, boundingBoxes)
+row = rows(box, boundingBoxes)
 
 # находим максимальное число коллонок, для создания итоговой таблички
 countcol = 0
@@ -53,10 +53,10 @@ center = np.array(center)
 center.sort()
 
 # получаем полностью отсортированный (теперь и слева направо) список ячеек
-finalboxes = package.rows_sort(row, countcol, center)
+finalboxes = rows_sort(row, countcol, center)
 
 # получаем список с распознанными значениями из ячеек
-outer = package.recognition(finalboxes, bitnot)
+outer = recognition(finalboxes, bitnot)
 
 # кладём данные в excel таблицу
-package.data_to_excel(outer, row, countcol)
+data_to_excel(outer, row, countcol)
